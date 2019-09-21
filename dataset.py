2c19435d044e3e485e0df2962f2211404956adca
Xinkang Jia <xk.jackson@hotmail.com>
2019-09-21 20:23:40 +0800

    Initial commit

---
+++ dataset.py
@@ -0,0 +1,146 @@
+import torch.utils.data as data
+import torchvision.transforms as transforms
+from PIL import Image
+import PIL
+import os
+import os.path
+import pickle
+import random
+import numpy as np
+import pandas as pd
+import collections
+
+import torch.utils.data as data
+from PIL import Image
+import os
+import os.path
+import six
+import string
+import sys
+import torch
+
+IMG_EXTENSIONS = ['.jpg', '.JPG', '.jpeg', '.JPEG',
+                  '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP']
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+
+def get_imgs(img_path, imsize, bbox=None,
+             transform=None):
+    img = Image.open(img_path).convert('RGB')
+    width, height = img.size
+    if bbox is not None:
+        r = int(np.maximum(bbox[2], bbox[3]) * 0.75)
+        center_x = int((2 * bbox[0] + bbox[2]) / 2)
+        center_y = int((2 * bbox[1] + bbox[3]) / 2)
+        y1 = np.maximum(0, center_y - r)
+        y2 = np.minimum(height, center_y + r)
+        x1 = np.maximum(0, center_x - r)
+        x2 = np.minimum(width, center_x + r)
+        img = img.crop([x1, y1, x2, y2])
+
+    if transform is not None:
+        img = transform(img)
+
+    return img
+
+
+class TextDataset(data.Dataset):
+    def __init__(self, data_dir, split='train', filenames=None, embedding_type='cnn-rnn', base_size=64, transform=None, target_transform=None):
+        self.transform = transform
+        self.target_transform = target_transform
+
+        self.imsize = []
+
+        self.data = []
+        self.data_dir = data_dir
+        if data_dir.find('birds') != -1:
+            self.bbox = self.load_bbox()
+        else:
+            self.bbox = None
+        split_dir = os.path.join(data_dir, split)
+
+        self.filenames = filenames
+        self.class_id = self.load_class_id(split_dir, len(self.filenames))
+        self.iterator = self.prepair_training_pairs
+
+    def load_bbox(self):
+        data_dir = self.data_dir
+        bbox_path = os.path.join(data_dir, 'CUB_200_2011/bounding_boxes.txt')
+        df_bounding_boxes = pd.read_csv(bbox_path,
+                                        delim_whitespace=True,
+                                        header=None).astype(int)
+        #
+        filepath = os.path.join(data_dir, 'CUB_200_2011/images.txt')
+        df_filenames = \
+            pd.read_csv(filepath, delim_whitespace=True, header=None)
+        filenames = df_filenames[1].tolist()
+        print('Total filenames: ', len(filenames), filenames[0])
+        #
+        filename_bbox = {img_file[:-4]: [] for img_file in filenames}
+        numImgs = len(filenames)
+        for i in range(0, numImgs):
+            # bbox = [x-left, y-top, width, height]
+            bbox = df_bounding_boxes.iloc[i][1:].tolist()
+
+            key = filenames[i][:-4]
+            filename_bbox[key] = bbox
+        #
+        return filename_bbox
+
+
+    def load_class_id(self, data_dir, total_num):
+        if os.path.isfile(data_dir + '/class_info.pickle'):
+            with open(data_dir + '/class_info.pickle', 'rb') as f:
+                class_id = pickle.load(f, encoding='latin1')
+        else:
+            class_id = np.arange(total_num)
+        set_class = list(set(class_id))
+        label2id = collections.defaultdict(str)
+        for i in range(len(set_class)):
+            label2id[set_class[i]] = i
+
+        return label2id
+
+
+    def prepair_training_pairs(self, index):
+        key = self.filenames[index]
+        id = int(key.split('.')[0])
+        label = self.class_id[id]
+        if self.bbox is not None:
+            bbox = self.bbox[key]
+            data_dir = '%s/CUB_200_2011' % self.data_dir
+        else:
+            bbox = None
+            data_dir = self.data_dir
+        img_name = '%s/images/%s.jpg' % (data_dir, key)
+        imgs = get_imgs(img_name, self.imsize,
+                        bbox, self.transform)
+
+        return imgs, label
+
+    def prepair_test_pairs(self, index):
+        key = self.filenames[index]
+
+        if self.bbox is not None:
+            bbox = self.bbox[key]
+            data_dir = '%s/CUB_200_2011' % self.data_dir
+        else:
+            bbox = None
+            data_dir = self.data_dir
+        captions = self.captions[key]
+        attribute_value = self.all_info[index]['attribute_value']
+        embeddings = self.embeddings[index, :, :]
+        img_name = '%s/images/%s.jpg' % (data_dir, key)
+        imgs = get_imgs(img_name, self.imsize,
+                        bbox, self.transform)
+
+        return imgs, embeddings, key, captions, attribute_value  # captions
+
+    def __getitem__(self, index):
+        return self.iterator(index)
+
+    def __len__(self):
+        return len(self.filenames)
